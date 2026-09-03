1137daf0717fc6580be624bf510f72793231b8ad
Peter Richards <peter.richards@liferay.com>
2026-09-03 16:07:07 +0100

    fix(sdk): bridge missing this.rest._chunkArray in LiferayService wrapper
    - subclass LiferayService in services/liferay/index.cjs to delegate this.rest._chunkArray
    - fixes TypeError: this.rest._chunkArray is not a function during deleteByFilter batch teardown
    - closes #574

--- client-extensions/ai-commerce-accelerator-microservice/services/liferay/index.cjs
+++ client-extensions/ai-commerce-accelerator-microservice/services/liferay/index.cjs
@@ -1,3 +1,32 @@
-const { LiferayService } = require('@liferay/accelerator-sdk');
+const {
+  LiferayService: BaseLiferayService,
+} = require('@liferay/accelerator-sdk');
+
+class LiferayService extends BaseLiferayService {
+  constructor(ctx) {
+    super(ctx);
+    if (this.rest && typeof this.rest._chunkArray !== 'function') {
+      this.rest._chunkArray = (arr, size) => {
+        if (
+          this.rest.batch &&
+          typeof this.rest.batch._chunkArray === 'function'
+        ) {
+          return this.rest.batch._chunkArray(arr, size);
+        }
+        if (
+          this.rest.batchDelete &&
+          typeof this.rest.batchDelete._chunkArray === 'function'
+        ) {
+          return this.rest.batchDelete._chunkArray(arr, size);
+        }
+        const chunks = [];
+        for (let i = 0; i < (arr || []).length; i += size) {
+          chunks.push(arr.slice(i, i + size));
+        }
+        return chunks;
+      };
+    }
+  }
+}
 
 module.exports = { LiferayService };
