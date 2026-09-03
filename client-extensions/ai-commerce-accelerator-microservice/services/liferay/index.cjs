a8d5c3410c66b4231824ea80c1b3fcf15783447a
Peter Richards <peter.richards@liferay.com>
2026-09-03 20:21:08 +0100

    feat(microservice): upgrade @liferay/accelerator-sdk to v0.8.2 and remove _chunkArray workaround

--- client-extensions/ai-commerce-accelerator-microservice/services/liferay/index.cjs
+++ client-extensions/ai-commerce-accelerator-microservice/services/liferay/index.cjs
@@ -1,32 +1,3 @@
-const {
-  LiferayService: BaseLiferayService,
-} = require('@liferay/accelerator-sdk');
-
-class LiferayService extends BaseLiferayService {
-  constructor(ctx) {
-    super(ctx);
-    if (this.rest && typeof this.rest._chunkArray !== 'function') {
-      this.rest._chunkArray = (arr, size) => {
-        if (
-          this.rest.batch &&
-          typeof this.rest.batch._chunkArray === 'function'
-        ) {
-          return this.rest.batch._chunkArray(arr, size);
-        }
-        if (
-          this.rest.batchDelete &&
-          typeof this.rest.batchDelete._chunkArray === 'function'
-        ) {
-          return this.rest.batchDelete._chunkArray(arr, size);
-        }
-        const chunks = [];
-        for (let i = 0; i < (arr || []).length; i += size) {
-          chunks.push(arr.slice(i, i + size));
-        }
-        return chunks;
-      };
-    }
-  }
-}
+const { LiferayService } = require('@liferay/accelerator-sdk');
 
 module.exports = { LiferayService };
